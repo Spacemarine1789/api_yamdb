6f4c38fa88f915b6ee81494b3eec249647b8aa16
Azamat <BzhukovAA@gmail.com>
2022-09-21 11:03:05 +0300

    title, genre, category

--- api_yamdb/Review/models.py
+++ api_yamdb/Review/models.py
@@ -1,16 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
 
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name='Название категории')
+    slug = models.SlugField(unique=True, verbose_name='Адрес')
+
+    class Meta:
+        ordering = ('-name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(
+        'Наименование',
+        help_text='Введите наименование'
+    )
+    year = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='title',
+        blank=True,
+        null=True,
+        verbose_name='Категория',
+        help_text='Категория, к которой будет относиться произведение'
+    )
+
+    class Meta:
+        ordering = ('-year',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name[:10]
+
+
 class Review(models.Model):
     title_id = models.ForeignKey(
-        Title, 
+        Title,
         on_delete=models.CASCADE,
         related_name='reviews',
     )
     text = models.TextField()
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         related_name='reviews'
     )
@@ -26,13 +72,13 @@ class Review(models.Model):
 
 class Comment(models.Model):
     review_id = models.ForeignKey(
-        Review, 
+        Review,
         on_delete=models.CASCADE,
         related_name='сomments',
     )
     text = models.TextField()
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         related_name='сomments'
     )
@@ -40,4 +86,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
