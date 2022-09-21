c5385e95a01ab4a1d78d8f375103f1759862070f
Azamat <BzhukovAA@gmail.com>
2022-09-21 20:32:18 +0300

    title_genre_category

--- api_yamdb/Review/models.py
+++ api_yamdb/Review/models.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
-User = get_user_model()
 
 
 class Genre(models.Model):
@@ -38,7 +35,7 @@ class Title(models.Model):
         help_text='Введите наименование'
     )
     year = models.DateTimeField(
-        'Дата публикации',
+        'Год публикации',
         auto_now_add=True
     )
     category = models.ForeignKey(
@@ -64,45 +61,8 @@ class Title(models.Model):
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
 
+    def get_year(self):
+        return self.year.year
+
     def __str__(self):
         return self.name[:10]
-
-
-class Review(models.Model):
-    title_id = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-    )
-    text = models.TextField()
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
-    score = models.IntegerField(
-        # MaxValueValidator=10,
-        # MinValueValidator=1,
-    )
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-
-    def __str__(self):
-        return self.text
-
-
-class Comment(models.Model):
-    review_id = models.ForeignKey(
-        Review,
-        on_delete=models.CASCADE,
-        related_name='сomments',
-    )
-    text = models.TextField()
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='сomments'
-    )
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-
-    def __str__(self):
-        return self.text
