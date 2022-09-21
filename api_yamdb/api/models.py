203815543236e034ee41fc554a61a453c77b309a
Андрей Соломатин <aisolomatin@inbox.ru>
2022-09-21 15:34:01 +0300

    Initail User

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+ROLES = (
+    ('user', 'Пользователь'),
+    ('admin','Админ'),
+    ('moderator','Модератор'),
+)
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    ),
+    role = models.CharField(max_length=80, choices=ROLES)
