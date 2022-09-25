8fc3de3a94db5281224e5eb97beff5626191adfb
Azamat <BzhukovAA@gmail.com>
2022-09-25 15:25:45 +0300

    genre_category_titles validation date

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,7 +4,13 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     """Permissions for change or delete Admin only"""
     message = 'Данное действие разрешено только администратору!'
+
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in SAFE_METHODS:
             return True
-        return request.user.role == 'admin'
+
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == 'admin'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+import datetime
+
+from rest_framework import viewsets
 
 from Review.models import Category, Genre, Title
 from .permissions import IsAdminOrReadOnly
@@ -9,20 +11,25 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     lookup_field = 'slug'
     serializer_class = CategorySerializer
-    permission_classes = (
-        permissions.IsAuthenticatedOrReadOnly, IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class GenriesViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     lookup_field = 'slug'
     serializer_class = GenreSerializer
-    permission_classes = (
-        permissions.IsAuthenticatedOrReadOnly, IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class TitlesViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitlesSerializer
-    permission_classes = (
-        permissions.IsAuthenticatedOrReadOnly, IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def validate(self, data):
+        """
+        Check that the current year is before entered year.
+        """
+        if data['year'] >= int(datetime.datetime.now().year):
+            raise serializers.ValidationError("Введенная дата больше текущей")
+        return data
